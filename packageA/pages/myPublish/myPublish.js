fe6558c2d375ce624b896c2db1c780ad3b602753
P1Kaj1uu <891523233@qq.com>
2024-03-12 22:37:55 +0800

    feat: 我的订单、我的发布

--- packageA/pages/myPublish/myPublish.js
+++ packageA/pages/myPublish/myPublish.js
@@ -1,13 +1,88 @@
 // packageA/pages/myPublish/myPublish.js
+var app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    userInfo: null,
+    authorGoodsList: []
   },
-
+  // 跳转详情
+  toDetailFn(e) {
+    let id = e.currentTarget.dataset.goodsid;
+    wx.navigateTo({
+      url: '../goodDetail/goodDetail?goodsId=' + id
+    });
+  },
+  // 下架
+  deteleFn(e) {
+    let that = this;
+    let goodsId = e.currentTarget.dataset.goodsid;
+    wx.request({
+      url: app.globalData.siteBaseUrl + '/goods/delete?goodsId=' + goodsId,
+      method: 'DELETE',
+      header: {
+        'Content-Type': 'application/json',
+        'X-Token': that.data.userInfo.token
+      },
+      success: function(res) {
+        console.log('下架约拍--->', res)
+        if (res.data.code === 401) {
+          wx.showToast({
+            title: res.data.msg,
+            icon: 'none',
+            duration: 2000,
+          })
+          wx.setStorageSync('userInfo', null);
+          wx.navigateTo({
+            url: '../../../pages/login/login',
+          })
+          return;
+        }
+        wx.showToast({
+          title: '下架成功',
+          icon: 'none',
+          duration: 2000,
+        })
+        that.getMyPublish();
+      }
+    })
+  },
+  // 获取我的发布
+  getMyPublish() {
+    let that = this;
+    wx.request({
+      url: app.globalData.siteBaseUrl + '/goods/find',
+      method: 'GET',
+      header: {
+        'Content-Type': 'application/json',
+        'X-Token': that.data.userInfo.token
+      },
+      data: {
+        "userId": that.data.userInfo.id
+      },
+      success: function(res) {
+        console.log('我的发布--->', res)
+        if (res.data.code === 401) {
+          wx.showToast({
+            title: res.data.msg,
+            icon: 'none',
+            duration: 2000,
+          })
+          wx.setStorageSync('userInfo', null);
+          wx.navigateTo({
+            url: '../../../pages/login/login',
+          })
+          return;
+        }
+        that.setData({
+          authorGoodsList: res.data.data
+        })
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -26,7 +101,10 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow() {
-
+    this.setData({
+      userInfo: wx.getStorageSync('userInfo')
+    })
+    this.getMyPublish();
   },
 
   /**
--- packageA/pages/myPurchase/myPurchase.js
+++ packageA/pages/myPurchase/myPurchase.js
@@ -1,13 +1,85 @@
 // packageA/pages/myPurchase/myPurchase.js
+var app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    userInfo: null,
+    purchaseGoodsList: [],
+    pageNum: 1,
+    pageSize: 100
   },
-
+  // 删除订单
+  deteleFn(e) {
+    let that = this;
+    let recordId = e.currentTarget.dataset.recordid;
+    wx.request({
+      url: app.globalData.siteBaseUrl + '/record/delete?recordId=' + recordId,
+      method: 'DELETE',
+      header: {
+        'Content-Type': 'application/json',
+        'X-Token': that.data.userInfo.token
+      },
+      success: function(res) {
+        console.log('删除订单--->', res)
+        if (res.data.code === 401) {
+          wx.showToast({
+            title: res.data.msg,
+            icon: 'none',
+            duration: 2000,
+          })
+          wx.setStorageSync('userInfo', null);
+          wx.navigateTo({
+            url: '../../../pages/login/login',
+          })
+          return;
+        }
+        wx.showToast({
+          title: '删除成功',
+          icon: 'none',
+          duration: 2000,
+        })
+        that.getUserPurchase();
+      }
+    })
+  },
+  // 获取当前用户的订单
+  getUserPurchase() {
+    let that = this;
+    wx.request({
+      url: app.globalData.siteBaseUrl + '/record/find',
+      method: 'GET',
+      header: {
+        'Content-Type': 'application/json',
+        'X-Token': that.data.userInfo.token
+      },
+      data: {
+        "userId": that.data.userInfo.id,
+        "pageNum": that.data.pageNum,
+        "pageSize": that.data.pageSize
+      },
+      success: function(res) {
+        console.log('我的订单--->', res)
+        if (res.data.code === 401) {
+          wx.showToast({
+            title: res.data.msg,
+            icon: 'none',
+            duration: 2000,
+          })
+          wx.setStorageSync('userInfo', null);
+          wx.navigateTo({
+            url: '../../../pages/login/login',
+          })
+          return;
+        }
+        that.setData({
+          purchaseGoodsList: res.data.data.list
+        })
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -26,7 +98,10 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow() {
-
+    this.setData({
+      userInfo: wx.getStorageSync('userInfo')
+    })
+    this.getUserPurchase();
   },
 
   /**
